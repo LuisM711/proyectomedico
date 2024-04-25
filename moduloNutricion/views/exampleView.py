799658a6322bfb3ede0c5a9e436ac702ab79d7bd
puppy09 <zepedaramireztics@gmail.com>
2024-04-25 16:53:12 -0700

    id de pestanitas corregido

--- moduloNutricion/views/exampleView.py
+++ moduloNutricion/views/exampleView.py
@@ -2,4 +2,5 @@ from django.shortcuts import render, redirect
 from django.views import View
 class nutriologo(View):
     def get(self, request):
+        
         return render(request, 'pruebaNutri.html')
