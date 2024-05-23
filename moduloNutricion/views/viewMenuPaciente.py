de854181e138e2b134b84e6f2717b7e2606eec54
puppy09 <zepedaramireztics@gmail.com>
2024-05-23 01:13:56 -0700

    correccion visualizacion de menu del paciente

--- moduloNutricion/views/viewMenuPaciente.py
+++ moduloNutricion/views/viewMenuPaciente.py
@@ -9,12 +9,20 @@ class Menu_paciente(View):
     def get(self, request):
         auxUsuario = Usuario.objects.get(id_usuario=request.user)
         pacienteBien = Paciente.objects.get(id_usuario=auxUsuario)
-        auxMenu = Menu_Bien.objects.filter(paciente=pacienteBien).order_by('fecha').first()
-        auxMenu2 = auxMenu.menu
-       # print(auxMenu2)
-        menuPaciente = json.loads(auxMenu2)
-        data={
-            'paciente': pacienteBien,
-            'menu': menuPaciente
-        }
+        auxMenu = Menu_Bien.objects.filter(paciente=pacienteBien).order_by('-fecha').first()
+        #Si encuentra un registro de asignacion de menu asignado a ese paciente
+        if auxMenu:
+            #Se jala unicamente el menu, que ha sido guardado como un json
+            auxMenu2 = auxMenu.menu
+            #print(auxMenu2)
+            menuPaciente = json.loads(auxMenu2)
+            data={
+                'paciente': pacienteBien,
+                'menu': menuPaciente
+            }
+        else:
+            data={
+                'paciente': pacienteBien,
+                'menu':None
+            }
         return render(request,'menuPaciente.html', data)
