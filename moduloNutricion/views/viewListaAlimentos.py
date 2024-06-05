add41756050161102781f35c6fd248741eb535c3
Luis Mario <43504168+LuisM711@users.noreply.github.com>
2024-06-05 16:57:33 -0700

    asd

--- moduloNutricion/views/viewListaAlimentos.py
+++ moduloNutricion/views/viewListaAlimentos.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views import View
 import json
+import sys
 from django.http import JsonResponse
 from moduloNutricion.models.modelAlimento import *
 
@@ -12,8 +13,9 @@ class Lista_Alimentos(View):
 def fetch_category_data(request):
    if request.method == 'GET':
       category = request.GET.get('category')
+      print(category)
       if category:
-         alimentos = Alimento.objects.filter(tipo__tipo_nombre=category) 
+         alimentos = Alimento.objects.filter(tipo__id=category)
          data = list(alimentos.values())
          return JsonResponse(data, safe=False)
    return JsonResponse({"error":"Category not found."}, status=400)
